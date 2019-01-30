6e2e23f37b6bf97a271fbfc641bc3c4a037b1483
Krystle Justine <KrystleJustine@gmail.com>
2019-01-30 13:21:16 -0500

    Done.

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,51 @@
+class Book
 
+  attr_accessor(:author, :page_count, :genre)
+  attr_reader(:title)
+  def initialize (title)
+  @title = title
+  end
+
+  # Sets up book method
+  def book
+
+  end
+
+  # def title
+  #   @title = "And Then There Were None"
+  # end
+
+  # # Setter method for author (note equal sign)...
+  # def author= (author)
+  #   @author = author
+  # end
+  #
+  # # Getter for author... just returns author
+  # def author
+  #   @author
+  # end
+ # Setter for page count
+ #  def page_count= (num)
+ #    @page_count = num
+ #  end
+ # # Getter for page count
+ #  def page_count
+ #    @page_count
+ #  end
+ #
+ #  # Setter for genre
+ #  def genre= (genre)
+ #    @genre= genre
+ #  end
+ #
+ #  # Getter for genre
+ #  def genre
+ #    @genre
+ #  end
+
+  # Method that makes our book do something
+  def turn_page
+    puts "Flipping the page...wow, you read fast!"
+  end
+end
 
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,23 @@
-# Make your shoe class here!
+class Shoe
+
+  attr_accessor(:color, :size, :material, :condition )
+  attr_reader(:brand)
+
+  def initialize (brand)
+  @brand = brand
+  end
+
+  def cobble
+    # We're already inside cobble... takes condition and changes it to new
+    @condition = ("new")
+
+    # 
+    puts "Your shoe is as good as new!"
+  end
+
+  def condition=(tattered)
+    @condition = tattered
+  end
+
+  #condition = ("new")
+end
